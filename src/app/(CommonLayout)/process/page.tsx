421ab2dbf3a26e74c50e991a6d91fe31c2ea326d
Raiyan109 <kraiyan109@gmail.com>
2025-02-03 10:56:15 +0600

    active route

---
+++ src/app/(CommonLayout)/process/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProcessPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ProcessPage
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
+'use client'
 import logo from '@/assets/jodi logo.png'
 import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+    const pathname = usePathname();
+
     return (
         <div className='flex justify-between items-center pl-[92px] pr-[69px] pt-[102px]'>
             <Image
@@ -12,11 +17,29 @@ const Navbar = () => {
                 className='w-[162px] h-[144px]'
             />
             <div className='flex items-center gap-[20px]'>
-                <button className='w-[141px] h-[60px] rounded-[40px] border border-primary text-[20px] font-dmSans'>Home</button>
-                <button className='w-[141px] h-[60px] rounded-[40px] border border-primary text-[20px] font-dmSans'>About Us</button>
-                <button className='w-[141px] h-[60px] rounded-[40px] border border-primary text-[20px] font-dmSans'>Process</button>
-                <button className='w-[141px] h-[60px] rounded-[40px] border border-primary text-[20px] font-dmSans'>Projects</button>
-                <button className='w-[141px] h-[60px] rounded-[40px] border border-primary text-[20px] font-dmSans'>Contact</button>
+                <Link href='/' >
+                    <button className={`w-[141px] h-[60px] rounded-[40px] border border-primary ${pathname === "/" ? 'bg-primary' : 'bg-transparent'} text-[20px] font-dmSans`}>Home</button>
+                </Link>
+                <Link href='/about'>
+                    <button className={`w-[141px] h-[60px] rounded-[40px] border border-primary ${pathname === "/about" ? 'bg-primary' : 'bg-transparent'} text-[20px] font-dmSans`}>
+                        About Us
+                    </button>
+                </Link>
+                <Link href='/process'>
+                    <button className={`w-[141px] h-[60px] rounded-[40px] border border-primary ${pathname === "/process" ? 'bg-primary' : 'bg-transparent'} text-[20px] font-dmSans`}>
+                        Process
+                    </button>
+                </Link>
+                <Link href='/projects'>
+                    <button className={`w-[141px] h-[60px] rounded-[40px] border border-primary ${pathname === "/projects" ? 'bg-primary' : 'bg-transparent'} text-[20px] font-dmSans`}>
+                        Projects
+                    </button>
+                </Link>
+                <Link href='/contact'>
+                    <button className={`w-[141px] h-[60px] rounded-[40px] border border-primary ${pathname === "/contact" ? 'bg-primary' : 'bg-transparent'} text-[20px] font-dmSans`}>
+                        Contact
+                    </button>
+                </Link>
             </div>
         </div>
     )
