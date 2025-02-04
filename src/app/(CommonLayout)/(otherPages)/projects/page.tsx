af6d81af5c83634e58af13261e44a5fc8acefa8e
Raiyan109 <kraiyan109@gmail.com>
2025-02-04 14:58:01 +0600

    my latest projects part of projects page

--- src/app/(CommonLayout)/(otherPages)/projects/page.tsx
+++ src/app/(CommonLayout)/(otherPages)/projects/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import arrow from '@/assets/projectsArrow.png'
+import Image from 'next/image'
 
 const ProjectsPage = () => {
     return (
-        <div>
-
+        <div className='px-[70px] mt-14'>
+            <div className='relative'>
+                <h3 className='font-belle text-[100px] text-secondary uppercase'>Projects</h3>
+                <h1 className='uppercase text-olive font-fascinate text-[200px] max-w-[1300px] leading-[200px]'>My latest projects</h1>
+                <Image
+                    src={arrow}
+                    width={200}
+                    height={200}
+                    alt='Lets start image'
+                    className='w-[184px] h-[70px] object-contain absolute left-64 top-9'
+                />
+            </div>
         </div>
     )
 }
