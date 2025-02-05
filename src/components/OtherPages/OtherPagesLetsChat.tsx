889ef5752df71c451e8651514be06aaf8b50d33c
Raiyan109 <kraiyan109@gmail.com>
2025-02-05 14:21:45 +0600

    otherpages letschat comp responsive

--- src/components/OtherPages/OtherPagesLetsChat.tsx
+++ src/components/OtherPages/OtherPagesLetsChat.tsx
@@ -4,35 +4,35 @@ import DoubleButton from '../Shared/DoubleButton'
 
 const OtherPagesLetsChat = () => {
     return (
-        <div className='mx-auto max-w-[1210px] flex items-center my-[78px]'>
+        <div className='mx-auto max-w-[1210px] flex flex-col lg:flex-row items-center my-[78px]'>
             <div className='relative'>
-                <h3 className='font-belle text-[100px] text-secondary uppercase'>Contact me</h3>
-                <h1 className='uppercase text-olive font-fascinate text-[200px] max-w-[640px] leading-[200px]'>Let's Chat</h1>
+                <h3 className='font-belle text-[50px] lg:text-[100px] text-secondary uppercase'>Contact me</h3>
+                <h1 className='uppercase text-olive font-fascinate text-[100px] lg:text-[200px] max-w-[640px] leading-none lg:leading-[200px]'>Let's Chat</h1>
                 <Image
                     src={arrow}
                     width={200}
                     height={200}
                     alt='Lets start image'
-                    className='w-[273px] h-[138px] object-contain absolute -right-6 -top-6'
+                    className='w-[173px] h-[38px] lg:w-[273px] lg:h-[138px] object-contain absolute right-40 top-5 lg:-right-6 lg:-top-6'
                 />
             </div>
             <div className='relative'>
-                <div className='w-[640px] h-[640px] rounded-full bg-lightBlue'>
+                <div className='w-[440px] h-[440px] lg:w-[640px] lg:h-[640px] rounded-full bg-lightBlue'>
 
                 </div>
-                <div className='absolute top-[22%] left-[85px]'>
+                <div className='absolute lg:top-[20%] lg:left-[85px] top-[20%] left-[100px]'>
                     <div className='flex items-center justify-center flex-col gap-[20px]'>
                         <input type="text"
                             placeholder='NAME'
-                            className='w-[460px] h-[60px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold'
+                            className='w-[230px] lg:w-[460px] h-[40px] lg:h-[60px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold'
                         />
                         <input type="text"
                             placeholder='EMAIL'
-                            className='w-[460px] h-[60px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold'
+                            className='w-[230px] lg:w-[460px] h-[40px] lg:h-[60px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold'
                         />
                         <textarea
                             placeholder='MESSAGE'
-                            className='w-[460px] h-[160px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold pt-[20px]'
+                            className='w-[230px] lg:w-[460px] h-[100px] lg:h-[160px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold pt-[20px]'
                         />
                         <div className=''>
                             <DoubleButton text='Send Message' bgColor='bg-brown' textColor='text-primary' />
