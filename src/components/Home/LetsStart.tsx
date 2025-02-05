2e18bfe32d751301f3d7da36c797ec15a61b1bda
Raiyan109 <kraiyan109@gmail.com>
2025-02-05 11:52:20 +0600

    lets start responsive

--- src/components/Home/LetsStart.tsx
+++ src/components/Home/LetsStart.tsx
@@ -4,11 +4,11 @@ import DoubleButton from '../Shared/DoubleButton'
 const LetsStart = () => {
     return (
         <div
-            className="mt-[400px] mb-32 mx-auto max-w-[1212px] h-[808px] bg-cover bg-center flex items-center justify-center"
+            className="mt-[400px] mb-32  max-w-[800px] lg:max-w-[1212px] h-[308px] lg:h-[808px] mx-2 lg:mx-auto bg-cover bg-center flex items-center justify-center"
             style={{ backgroundImage: `url(${letsStart.src})` }}
         >
             <div className='flex flex-col items-center justify-center gap-[50px] text-center'>
-                <h1 className='font-abel text-[100px]'>Let’s Start a Project</h1>
+                <h1 className='font-abel text-[50px] lg:text-[100px]'>Let’s Start a Project</h1>
                 <DoubleButton text='Start Project' bgColor='bg-secondary' textColor='text-olive' />
             </div>
         </div>
