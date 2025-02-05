e7c4dcc731606858d8529036f6e9894a24365b25
Raiyan109 <kraiyan109@gmail.com>
2025-02-05 10:13:08 +0600

    navbar responsive started

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -8,15 +8,15 @@ const Navbar = () => {
     const pathname = usePathname();
 
     return (
-        <div className='flex justify-between items-center pl-[92px] pr-[69px] pt-[102px]'>
+        <div className='flex justify-between items-center pl-5 pr-5 lg:pl-[92px] lg:pr-[69px] pt-7 lg:pt-[102px]'>
             <Image
                 src={logo}
                 width={100}
                 height={100}
                 alt='Jodi Logo Image'
-                className='w-[162px] h-[144px] object-contain'
+                className='w-20 h-20 lg:w-[162px] lg:h-[144px] object-contain'
             />
-            <div className='flex items-center gap-[20px]'>
+            <div className='lg:flex hidden items-center gap-[20px]'>
                 <Link href='/' >
                     <button className={`w-[141px] h-[60px] rounded-[40px] border border-primary ${pathname === "/" ? 'bg-primary' : 'bg-transparent'} text-[20px] font-dmSans`}>Home</button>
                 </Link>
