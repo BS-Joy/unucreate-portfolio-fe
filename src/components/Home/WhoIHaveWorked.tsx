fc8adb2c57e8af5f2892979d3a0c1b47c62c7e77
Raiyan109 <kraiyan109@gmail.com>
2025-02-05 11:20:42 +0600

    who I have worked with section responsive

--- src/components/Home/WhoIHaveWorked.tsx
+++ src/components/Home/WhoIHaveWorked.tsx
@@ -10,54 +10,54 @@ import worklogo5 from "@/assets/worklogo5.png"
 const WhoIHaveWorked = () => {
     return (
         <div className="mx-auto max-w-[884.15px] flex flex-col justify-center items-center pb-32">
-            <div className="border-t border-secondary w-[504px] "></div>
-            <h1 className="text-[48px] font-anton text-secondary text-center pt-[57px]">Who I have worked with </h1>
+            <div className="border-t border-secondary w-[304px] lg:w-[504px]"></div>
+            <h1 className="text-3xl lg:text-[48px] font-anton text-secondary text-center pt-[27px] lg:pt-[57px]">Who I have worked with </h1>
 
-            <div className="flex items-center gap-[49.65px] pb-[110px]">
+            <div className="grid grid-cols-2 lg:flex items-center gap-[29.65px] lg:gap-[49.65px] pb-[60px] lg:pb-[110px] mt-6 lg:mt-0">
                 <Image
                     src={worklogo1}
                     width={200}
                     height={200}
                     alt='Jodi Logo Image'
-                    className='w-[131.17px] h-[83.61px] object-contain'
+                    className='w-[91.17px] lg:w-[131.17px] h-[43.61px] lg:h-[83.61px] object-contain'
                 />
                 <Image
                     src={worklogo2}
                     width={200}
                     height={200}
                     alt='Jodi Logo Image'
-                    className='w-[131.17px] h-[83.61px] object-contain'
+                    className='w-[91.17px] lg:w-[131.17px] h-[43.61px] lg:h-[83.61px] object-contain'
                 />
                 <Image
                     src={worklogo3}
                     width={200}
                     height={200}
                     alt='Jodi Logo Image'
-                    className='w-[131.17px] h-[83.61px] object-contain'
+                    className='w-[91.17px] lg:w-[131.17px] h-[43.61px] lg:h-[83.61px] object-contain'
                 />
                 <Image
                     src={worklogo4}
                     width={200}
                     height={200}
                     alt='Jodi Logo Image'
-                    className='w-[131.17px] h-[83.61px] object-contain'
+                    className='w-[91.17px] lg:w-[131.17px] h-[43.61px] lg:h-[83.61px] object-contain'
                 />
                 <Image
                     src={worklogo5}
                     width={200}
                     height={200}
                     alt='Jodi Logo Image'
-                    className='w-[131.17px] h-[83.61px] object-contain'
+                    className='w-[91.17px] lg:w-[131.17px] h-[43.61px] lg:h-[83.61px] object-contain'
                 />
                 <Image
                     src={worklogo4}
                     width={200}
                     height={200}
                     alt='Jodi Logo Image'
-                    className='w-[131.17px] h-[83.61px] object-contain'
+                    className='w-[91.17px] lg:w-[131.17px] h-[43.61px] lg:h-[83.61px] object-contain'
                 />
             </div>
-            <div className="border-b border-secondary w-[504px] "></div>
+            <div className="border-b border-secondary w-[304px] lg:w-[504px]"></div>
         </div>
     )
 }
