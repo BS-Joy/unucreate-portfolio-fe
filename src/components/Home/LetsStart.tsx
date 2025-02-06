31fd1628b98a981af13ad5bcf5ad4044715093b7
Raiyan109 <kraiyan109@gmail.com>
2025-02-06 18:26:28 +0600

    animation ok

--- src/components/Home/LetsStart.tsx
+++ src/components/Home/LetsStart.tsx
@@ -1,5 +1,7 @@
+'use client'
 import letsStart from '@/assets/lets-start.png'
 import DoubleButton from '../Shared/DoubleButton'
+import { motion } from 'motion/react'
 
 const LetsStart = () => {
     return (
@@ -7,10 +9,14 @@ const LetsStart = () => {
             className="mt-[400px] mb-32  max-w-[800px] lg:max-w-[1212px] h-[308px] lg:h-[808px] mx-2 lg:mx-auto bg-cover bg-center flex items-center justify-center"
             style={{ backgroundImage: `url(${letsStart.src})` }}
         >
-            <div className='flex flex-col items-center justify-center gap-[50px] text-center'>
+            <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className='flex flex-col items-center justify-center gap-[50px] text-center'>
                 <h1 className='font-abel text-[50px] lg:text-[100px]'>Let’s Start a Project</h1>
                 <DoubleButton text='Start Project' bgColor='bg-secondary' textColor='text-olive' />
-            </div>
+            </motion.div>
         </div>
 
     )
--- src/components/Home/Welcome.tsx
+++ src/components/Home/Welcome.tsx
@@ -1,3 +1,4 @@
+'use client'
 import silverBallImg from '@/assets/silver ball.png'
 import jodiImg from '@/assets/jodi about image.png'
 import instagram from '@/assets/welcomeInstagram.png'
@@ -5,11 +6,16 @@ import mail from '@/assets/welcomeMail.png'
 import linkedin from '@/assets/welcomeLinkedin.png'
 import Image from 'next/image'
 import DoubleButton from '../Shared/DoubleButton'
+import { motion } from 'motion/react'
 
 const Welcome = () => {
     return (
         <div className='flex flex-col lg:flex-row justify-center items-center gap-[109.69px] my-[123.79]'>
-            <div className='flex flex-col items-center ml-4 lg:ml-44'>
+            <motion.div
+                initial={{ opacity: 0, x: -50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className='flex flex-col items-center ml-4 lg:ml-44'>
                 <div className='relative mb-[70px]'>
                     <Image
                         src={silverBallImg}
@@ -26,8 +32,13 @@ const Welcome = () => {
                     I’m Jodi Swaby, an NYC-based User experience designer AnD artist. I transform ideas into immersive digital and physical experiences that challenge the ordinary and spark curiosity. Rooted in user-centered design, I blend Data insights, play, and innovation to craft interactions that inspire, connect, and reimagine the human experience.</h1>
 
                 <DoubleButton text='Start Project' bgColor='bg-secondary' textColor='text-olive' />
-            </div>
-            <div className='flex flex-col items-center justify-center relative'>
+            </motion.div>
+
+            <motion.div
+                initial={{ opacity: 0, x: 50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className='flex flex-col items-center justify-center relative'>
                 <Image
                     src={jodiImg}
                     width={200}
@@ -58,7 +69,7 @@ const Welcome = () => {
                         className='w-[44.17px] h-[35.33px] object-contain'
                     />
                 </div>
-            </div>
+            </motion.div>
         </div>
     )
 }
