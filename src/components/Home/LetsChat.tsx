f3f9e2738dd07214d65b15f76c8c446c4da53eb0
Raiyan109 <kraiyan109@gmail.com>
2025-02-09 11:40:33 +0600

    lets chat done

--- src/components/Home/LetsChat.tsx
+++ src/components/Home/LetsChat.tsx
@@ -3,15 +3,27 @@ import silverBall from '@/assets/chatBall.png'
 import Image from 'next/image'
 import DoubleButton from '../Shared/DoubleButton'
 import { motion } from 'motion/react'
+import arrow from '@/assets/lets-chat-arrow.png'
 
 const LetsChat = () => {
     return (
-        <div className='mx-auto max-w-[1210px] flex flex-col lg:flex-row items-center mb-[78px]'>
+        <div className='mx-auto max-w-[1298px] flex flex-col lg:flex-row items-center mb-[78px] gap-[90px] relative'>
             <motion.h1
                 initial={{ opacity: 0, x: -50 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1, delay: 0.5 }}
-                className='uppercase text-olive font-anton text-[90px] lg:text-[200px] max-w-[640px] leading-[200px]'>Let's Chat</motion.h1>
+                className='uppercase text-olive font-anton text-[90px] lg:text-[128px] max-w-[640px] leading-[200px]'>Let's Chat</motion.h1>
+            <motion.div initial={{ opacity: 0, x: -50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 0.5 }} className='absolute top-20 left-80'>
+                <Image
+                    src={arrow}
+                    width={600}
+                    height={600}
+                    alt='arrow'
+                    className='w-[273px] h-[138px]'
+                />
+            </motion.div>
             <motion.div
                 initial={{ opacity: 0, x: 50 }}
                 whileInView={{ opacity: 1, x: 0 }}
@@ -19,24 +31,24 @@ const LetsChat = () => {
                 className='relative'>
                 <Image
                     src={silverBall}
-                    width={200}
-                    height={200}
+                    width={1000}
+                    height={1000}
                     alt='Lets start image'
-                    className='w-[350px] h-[400px] lg:w-[640px] lg:h-[640px]'
+                    className='w-[350px] h-[400px] lg:w-[640px] lg:h-[640px] object-contain'
                 />
-                <div className='absolute top-[12%] lg:top-[22%] left-[60px] lg:left-[60px]'>
+                <div className='absolute top-[12%] lg:top-[22%] left-[60px] lg:left-[110px]'>
                     <div className='flex items-center justify-center flex-col gap-[20px]'>
                         <input type="text"
                             placeholder='NAME'
-                            className='w-[230px] lg:w-[460px] h-[40px] lg:h-[60px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold'
+                            className='w-[230px] lg:w-[410px] h-[40px] lg:h-[60px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold'
                         />
                         <input type="text"
                             placeholder='EMAIL'
-                            className='w-[230px] lg:w-[460px] h-[40px] lg:h-[60px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold'
+                            className='w-[230px] lg:w-[410px] h-[40px] lg:h-[60px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold'
                         />
                         <textarea
                             placeholder='MESSAGE'
-                            className='w-[230px] lg:w-[460px] h-[100px] lg:h-[160px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold pt-[20px]'
+                            className='w-[230px] lg:w-[410px] h-[100px] lg:h-[160px] bg-white border-none outline-none rounded-[8px] placeholder:text-black placeholder:text-[16px] font-dmSans pl-[30px] shadow-lg font-semibold pt-[20px]'
                         />
                         <div className=''>
                             <DoubleButton text='Send Message' bgColor='bg-secondary' textColor='text-olive' />
--- src/components/Shared/DoubleButton.tsx
+++ src/components/Shared/DoubleButton.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const DoubleButton = ({ text, bgColor, textColor }: { text: string, bgColor: string, textColor: string }) => {
     return (
         <div className='relative'>
-            <button className={`w-[180px] lg:w-[280px] h-[60px] lg:h-[80px] ${bgColor} rounded-[40px] flex items-center justify-center uppercase font-dmSans ${textColor} text-[20px] z-10 relative`}>
+            <button className={`w-[180px] lg:w-[275px] h-[60px] lg:h-[80px] ${bgColor} rounded-[40px] flex items-center justify-center uppercase font-dmSans ${textColor} text-[20px] z-10 relative`}>
                 {text}
             </button>
-            <button className='w-[180px] lg:w-[280px] h-[60px] lg:h-[80px] bg-primary rounded-[40px] flex items-center justify-center uppercase font-dmSans text-olive text-[20px] absolute -bottom-2 left-2'>
+            <button className='w-[180px] lg:w-[275px] h-[60px] lg:h-[80px] bg-primary rounded-[40px] flex items-center justify-center uppercase font-dmSans text-olive text-[20px] absolute -bottom-2 left-2'>
 
             </button>
         </div>
