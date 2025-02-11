54795d80d940b3da4c14401d3a5173a8ac688f24
Raiyan109 <kraiyan109@gmail.com>
2025-02-11 11:51:17 +0600

    client change lets start text chage to comming soon

--- src/components/Home/LetsStart.tsx
+++ src/components/Home/LetsStart.tsx
@@ -6,16 +6,17 @@ import { motion } from 'motion/react'
 const LetsStart = () => {
     return (
         <div
-            className="mt-[400px] mb-32  max-w-[800px] md:max-w-[900px] lg:max-w-[1212px] h-[308px] lg:h-[808px] mx-2 lg:mx-auto bg-cover bg-center flex items-center justify-center"
+            className="mt-[350px] mb-32  max-w-[800px] md:max-w-[900px] lg:max-w-[1212px] h-[308px] lg:h-[808px] mx-2 lg:mx-auto bg-cover bg-center flex justify-center pt-20"
             style={{ backgroundImage: `url(${letsStart.src})` }}
         >
             <motion.div
                 initial={{ opacity: 0, y: -50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1, delay: 0.5 }}
-                className='flex flex-col items-center justify-center gap-[50px] text-center'>
-                <h1 className='font-abel text-[50px] lg:text-[100px]'>Let’s Start a Project</h1>
-                <DoubleButton text='Start Project' bgColor='bg-secondary' textColor='text-olive' />
+                className='flex flex-col items-center gap-[10px] text-center'>
+                <h1 className='font-anton text-[50px] lg:text-[128px] uppercase'>Coming Soon</h1>
+                <h1 className='font-abel text-[50px] lg:text-[100px] uppercase'>Art gallery</h1>
+                <DoubleButton text='Check out the space ' bgColor='bg-secondary' textColor='text-olive' />
             </motion.div>
         </div>
 
