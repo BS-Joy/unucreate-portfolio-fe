ec85b2d5198ae27addc3ae84f17afbf771bbedb1
Raiyan109 <kraiyan109@gmail.com>
2025-02-11 12:31:46 +0600

    typewritter effect with useeffect

--- src/components/Home/AnimatedWelcome.tsx
+++ src/components/Home/AnimatedWelcome.tsx
@@ -3,15 +3,18 @@ import { motion } from 'motion/react'
 import Image from 'next/image'
 import tv from '@/assets/tv.png'
 import silverBallImg from '@/assets/silver ball.png'
+import Typewriter from './TypeWriter'
 
 const AnimatedWelcome = () => {
+    const typeWrittingText = "Hi, thanks for stopping by. I'm Jodi Swaby—an NYC-based user experience designer who began her journey as a fine artist. Along the way, I picked up skills in business development, startups, and content creation/production, which all inform my unique approach to design. I transform ideas into immersive digital and physical experiences that gently break away from the ordinary and invite curiosity. Grounded in user-centered design, I blend data insights with a touch of play and thoughtful innovation. My aim is to create interactions that inspires and connect, reimagining the way we experience our world."
+
     return (
         <div className='flex flex-col lg:flex-row justify-center items-center gap-0 lg:gap-[109.69px] my-0 md:my-[123px]'>
             <motion.div
                 initial={{ opacity: 0, x: -50 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1, delay: 0.5 }}
-                className='flex flex-col items-center px-3 ml-0 lg:ml-44 relative'>
+                className='flex-1 flex flex-col items-center px-3 ml-0 lg:ml-44 relative'>
                 <div className='relative'>
                     <Image
                         src={silverBallImg}
@@ -42,10 +45,10 @@ const AnimatedWelcome = () => {
                 initial={{ opacity: 0, x: 50 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1, delay: 0.5 }}
-                className='flex flex-col items-center justify-center relative'>
-                <h1 className='font-abel text-[20px] leading-[40px] max-w-[1004px] text-center tracking-widest'>Hi, thanks for stopping by. I'm Jodi Swaby—an NYC-based user experience designer who began her journey as a fine artist.
-                    Along the way, I picked up skills in business development, startups, and content creation/production, which all inform my unique approach to design.
-                    I transform ideas into immersive digital and physical experiences that gently break away from the ordinary and invite curiosity. Grounded in user-centered design, I blend data insights with a touch of play and thoughtful innovation. My aim is to create interactions that inspires and connect, reimagining the way we experience our world. </h1>
+                className='flex-1 flex flex-col items-center justify-center relative'>
+                <h1 className='font-abel text-[20px] leading-[40px] max-w-[1004px] text-center tracking-widest'>
+                    <Typewriter text={typeWrittingText} delay={100} />
+                </h1>
             </motion.div>
         </div>
     )
