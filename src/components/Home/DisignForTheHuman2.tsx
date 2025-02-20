69fa4825130443f9ce34abd3d4db18442a612607
Raiyan109 <kraiyan109@gmail.com>
2025-02-20 17:06:09 +0600

    disignForTheHuman mobile

--- src/components/Home/DisignForTheHuman2.tsx
+++ src/components/Home/DisignForTheHuman2.tsx
@@ -11,15 +11,15 @@ const DisignForTheHuman2 = () => {
     const [activeIndex, setActiveIndex] = useState<number | null>(null);
     return (
         <div className="mx-auto max-w-[1400px] flex flex-col justify-center items-center pb-32">
-            <h1 className="text-3xl lg:text-[48px] font-anton text-secondary text-center pt-[27px] lg:pt-[57px] mb-[40px]">Let's work together</h1>
-            <div className="border-t border-secondary w-[304px] lg:w-[504px] mb-[40px]"></div>
+            <h1 className="text-[32px] lg:text-[48px] font-anton text-secondary text-center pt-[27px] lg:pt-[57px] mb-[24px] lg:mb-[40px] uppercase">Let's work together</h1>
+            <div className="border-t border-secondary w-[304px] lg:w-[504px] pb-[24px] lg:pb-[40px]"></div>
 
-            {/* Button */}
+            {/* Button large screen */}
             <motion.div
                 initial={{ opacity: 0, x: -50 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1, delay: 0.5 }}
-                className=' flex items-center justify-center'>
+                className='hidden lg:flex items-center justify-center'>
                 <div className='relative'>
                     <button className={`w-[180px] lg:w-[228px] h-[60px] lg:h-[80px] bg-secondary rounded-[40px] flex items-center justify-center uppercase font-anton text-[#333333] text-[20px] z-10 relative font-bold`}>
                         Let's Connect
@@ -30,22 +30,38 @@ const DisignForTheHuman2 = () => {
                 </div>
             </motion.div>
 
-            <div className="border-b border-secondary w-[304px] lg:w-[504px]  mt-[40px] mb-0 lg:mb-32"></div>
+            {/* Button mobile screen */}
+            <motion.div
+                initial={{ opacity: 0, x: -50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className='flex lg:hidden items-center justify-center'>
+                <div className='relative'>
+                    <button className={`w-[239px] lg:w-[228px] h-[48px] lg:h-[80px] bg-secondary rounded-[40px] flex items-center justify-center uppercase font-anton text-[#333333] text-[16px] z-10 relative font-medium`}>
+                        Let's Connect
+                    </button>
+                    <button className='w-[239px] lg:w-[228px] h-[48px] lg:h-[80px] bg-primary rounded-[40px] flex items-center justify-center uppercase font-anton text-olive text-[20px] absolute -bottom-2 left-2'>
+
+                    </button>
+                </div>
+            </motion.div>
+
+            <div className="border-b border-secondary w-[304px] lg:w-[504px]  mt-[24px] mb-10 lg:mb-32"></div>
 
             {/* TV Image */}
             <motion.div
                 initial={{ opacity: 0, x: 50 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1, delay: 0.5 }}
-                className='relative pb-[10px] lg:pb-[190px]'>
+                className='relative pb-[24px] lg:pb-[190px]'>
                 <Image
                     src={tv}
                     width={1000}
                     height={1000}
                     alt='TV'
-                    className='w-[270.81px] lg:w-[819.8px] h-[350px] lg:h-[535.41px] object-contain'
+                    className='w-[335px] lg:w-[819.8px] h-[210px] lg:h-[535.41px] object-contain'
                 />
-                <h1 className='text-[46px] lg:text-[96px] text-[#ee3322] font-arizonia max-w-[250px] lg:max-w-sm text-center absolute top-[40%] lg:top-[20%] -left-4 lg:left-[16%] leading-10 lg:leading-none'>My Aproach</h1>
+                <h1 className='text-[46px] lg:text-[96px] text-[#ee3322] font-arizonia max-w-[250px] lg:max-w-sm text-center absolute top-[29%] lg:top-[20%] left-3 lg:left-[16%] leading-10 lg:leading-none'>My Aproach</h1>
             </motion.div>
 
 
@@ -56,8 +72,8 @@ const DisignForTheHuman2 = () => {
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1, delay: 0.5 }}
                 className='flex items-center gap-[13px] relative'>
-                <h1 className='font-anton text-[30px] lg:text-[64px] text-olive max-w-[270px] lg:max-w-none'>Designing for the Human Experience</h1>
-                <div className='border-2 border-[#ee3322] rounded-full p-1 absolute bottom-2 right-1 lg:relative lg:flex items-center justify-center mt-8'>
+                <h1 className='font-anton text-center text-[32px] lg:text-[64px] text-olive max-w-[280px] lg:max-w-none uppercase'>Designing for the Human Experience</h1>
+                <div className='border-2 border-[#ee3322] rounded-full p-1 hidden absolute bottom-2 right-1 lg:relative lg:flex items-center justify-center mt-8'>
                     <Image
                         src={logo}
                         width={200}
@@ -70,7 +86,7 @@ const DisignForTheHuman2 = () => {
 
 
             {/* Cards */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[43px] mt-[80px]">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[20px] lg:gap-[43px] mt-[40px] lg:mt-[80px]">
                 {["Insight", "Play", "Innovation"].map((title, index) => (
                     <FlipCard
                         key={index}
@@ -108,7 +124,7 @@ const FlipCard = ({
 }) => {
     return (
         <div
-            className="w-[344px] h-[344px] perspective-1000"
+            className="w-[334px] h-[236px] perspective-1000"
             onMouseEnter={setActiveIndex} // Change active card when hovered
         >
             <motion.div
--- src/components/Home/ShowroomMobile.tsx
+++ src/components/Home/ShowroomMobile.tsx
@@ -14,29 +14,32 @@ const ShowroomMobile = () => {
                 className='w-[375px] h-[379px] object-cover'
             /> */}
             <div
-                className="w-[375px] h-[379px] bg-cover bg-center flex flex-col items-center justify-center"
-                style={{ backgroundImage: `url(${showroom1.src})` }} // Set the sky image as background
+                className="w-[375px] h-[379px] bg-cover bg-center flex flex-col justify-between items-center"
+                style={{ backgroundImage: `url(${showroom1.src})` }}
             >
-                <motion.div
-                    initial={{ opacity: 0, y: 50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 1, delay: 0.5 }}
-                    className="w-[250px] h-[109px] bg-primary flex flex-col items-center justify-center mx-auto gap-[12px]"
-                >
-                    <h3 className='text-[20px] text-[#ee3322] font-abel uppercase'>Art Gallery</h3>
-                    <h1 className='text-olive text-[32px] font-anton uppercase'>showroom</h1>
+                {/* Centered Motion Div */}
+                <div className="flex-grow pt-16 flex items-center justify-center w-full">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1, delay: 0.5 }}
+                        className="w-[250px] h-[109px] bg-primary flex flex-col items-center justify-center mx-auto gap-[12px]"
+                    >
+                        <h3 className='text-[20px] text-[#ee3322] font-abel uppercase'>Art Gallery</h3>
+                        <h1 className='text-olive text-[32px] font-anton uppercase'>showroom</h1>
+                    </motion.div>
+                </div>
 
-
-                </motion.div>
-                <div className='relative'>
-                    <button className={`w-[260px] lg:w-[260px] h-[48px] lg:h-[80px] bg-secondary rounded-[40px] flex items-center justify-center uppercase font-anton text-[#333333] text-[20px] z-10 relative `}>
+                {/* Bottom Button Section */}
+                <div className='relative flex items-end mb-[31px]'>
+                    <button className="w-[260px] lg:w-[260px] h-[48px] lg:h-[80px] bg-secondary rounded-[40px] flex items-center justify-center uppercase font-anton text-[#333333] text-[20px] z-10 relative">
                         Curious?
                     </button>
                     <button className='w-[260px] lg:w-[260px] h-[48px] lg:h-[80px] bg-primary rounded-[40px] flex items-center justify-center uppercase font-anton text-olive text-[20px] absolute -bottom-2 left-2'>
-
                     </button>
                 </div>
             </div>
+
         </div>
     )
 }
