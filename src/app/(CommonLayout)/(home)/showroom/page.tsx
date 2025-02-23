a39142cd56594ab4620f949d82c9950b17314fdd
Raiyan109 <kraiyan109@gmail.com>
2025-02-23 07:57:45 +0600

    showroom page responsive

--- src/app/(CommonLayout)/(home)/showroom/page.tsx
+++ src/app/(CommonLayout)/(home)/showroom/page.tsx
@@ -7,39 +7,6 @@ import { useRouter } from "next/navigation";
 import showroom1 from '@/assets/showroom1.png'
 import showroom2 from '@/assets/showroom2.png'
 
-const projects = [
-    {
-        id: 1,
-        title: 'The Giver – UX Research for Anonymous Gift-Giving Platform',
-        description: 'The Giver is an innovative, anonymous gift-giving platform designed to streamline charitable donations with a seamless user experience.',
-        image: projectImg,
-        myRole: 'Lead UX Researcher. I spearheaded the entire research process, including defining user personas, conducting in-depth interviews, analyzing data, and presenting actionable insights to the product and development teams.',
-        process: 'User Interviews: Conducted 25 interviews with potential donors to gather insights on motivations and preferences.Competitive Analysis: Evaluated 10 platforms to identify user experience gaps.Personas & Journey Mapping: Created detailed user personas and journey maps to illustrate the donor experience.Synthesis & Reporting: Delivered a comprehensive report with clear recommendations.',
-        deliverables: 'User Personas, User Journey Maps, Research Report',
-        outcome: 'Aligned stakeholders on the app’s unique value proposition (social media integration).Informed strategic design decisions, including customizable donation flows and privacy controls.Established a user-driven roadmap, receiving positive early feedback on the platform’s ease of use.'
-    },
-    {
-        id: 2,
-        title: 'Blendyd Drive – UX/UI Design for Blendyd Studios',
-        description: 'Blendyd Studios is a software development firm modernizing recruitment in the trucking industry using AI and Machine Learning.',
-        image: projectImg,
-        myRole: 'Lead UX Researcher. I spearheaded the entire research process, including defining user personas, conducting in-depth interviews, analyzing data, and presenting actionable insights to the product and development teams.',
-        process: 'User Interviews: Conducted 25 interviews with potential donors to gather insights on motivations and preferences.Competitive Analysis: Evaluated 10 platforms to identify user experience gaps.Personas & Journey Mapping: Created detailed user personas and journey maps to illustrate the donor experience.Synthesis & Reporting: Delivered a comprehensive report with clear recommendations.',
-        deliverables: 'User Personas, User Journey Maps, Research Report',
-        outcome: 'Aligned stakeholders on the app’s unique value proposition (social media integration).Informed strategic design decisions, including customizable donation flows and privacy controls.Established a user-driven roadmap, receiving positive early feedback on the platform’s ease of use.'
-    },
-    {
-        id: 3,
-        title: 'Bumble x Jodi Swaby Art Collaboration',
-        description: 'In partnership with Bumble, I explored the nuances of dating through an art project titled “The Dance of Dating.”',
-        image: projectImg,
-        myRole: 'Lead UX Researcher. I spearheaded the entire research process, including defining user personas, conducting in-depth interviews, analyzing data, and presenting actionable insights to the product and development teams.',
-        process: 'User Interviews: Conducted 25 interviews with potential donors to gather insights on motivations and preferences.Competitive Analysis: Evaluated 10 platforms to identify user experience gaps.Personas & Journey Mapping: Created detailed user personas and journey maps to illustrate the donor experience.Synthesis & Reporting: Delivered a comprehensive report with clear recommendations.',
-        deliverables: 'User Personas, User Journey Maps, Research Report',
-        outcome: 'Aligned stakeholders on the app’s unique value proposition (social media integration).Informed strategic design decisions, including customizable donation flows and privacy controls.Established a user-driven roadmap, receiving positive early feedback on the platform’s ease of use.'
-    },
-]
-
 
 const ShowroomPage = () => {
 
@@ -65,7 +32,7 @@ const ShowroomPage = () => {
             </div>
 
             {/* Content and images */}
-            <div className="flex flex-col lg:flex-row mb-[340px] pt-[100px] lg:pt-[100px]">
+            <div className="flex flex-col lg:flex-row pt-[30px] lg:pt-[80px]">
                 {/* Left */}
                 <div className='flex-1 relative'>
                     <Image
@@ -73,7 +40,7 @@ const ShowroomPage = () => {
                         width={1200}
                         height={1200}
                         alt='Lets start image'
-                        className='w-full h-[1054px] object-cover '
+                        className='w-full h-[500px] lg:h-[1054px] object-cover'
                     />
 
                     {/* Green are */}
@@ -81,9 +48,9 @@ const ShowroomPage = () => {
                         initial={{ opacity: 0, x: -50 }}
                         whileInView={{ opacity: 1, x: 0 }}
                         transition={{ duration: 1, delay: 0.5 }}
-                        className='w-[380px] lg:w-[510px] h-[242px] bg-primary absolute left-0 top-[20%] flex flex-col items-center justify-center gap-5 uppercase'>
+                        className='w-[380px] lg:w-[440px] xl:w-[510px] h-[242px] bg-primary absolute left-12 sm:left-[23%] md:left-[30%] lg:left-0 top-[25%] lg:top-[20%] flex flex-col items-center justify-center gap-5 uppercase'>
                         <h3 className='text-[24px] text-[#ee3322] font-abel'>Art Gallery</h3>
-                        <h1 className='text-olive text-[40px] md:text-[90px] font-anton'>showroom</h1>
+                        <h1 className='text-olive text-[40px] lg:text-[90px] font-anton'>showroom</h1>
                     </motion.div>
                 </div>
 
